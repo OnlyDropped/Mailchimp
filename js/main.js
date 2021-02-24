4899e5413fc801f33841bdcc7d23fb48fb2e54a4
OnlyDropped <asheburenkov@yandex.ru>
2021-02-24 13:28:17 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(function () {
+  $('.partners__inner').slick({
+    arrows: true,
+    dots: true,
+    prevArrow: '<div class="prev"></div>',
+    nextArrow: '<div class="next"></div>',
+  });
+});
